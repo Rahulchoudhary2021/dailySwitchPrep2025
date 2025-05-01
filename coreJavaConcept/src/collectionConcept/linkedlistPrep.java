b151cc4e25e6ea4fe58172431665609244e05b98
rahul.sureshkumar <rahul.sureshkumar@LPT4711.newgen.co.in>
2025-05-02 00:49:27 +0530

    Array List topic

--- coreJavaConcept/src/collectionConcept/linkedlistPrep.java
+++ coreJavaConcept/src/collectionConcept/linkedlistPrep.java
@@ -1,7 +1,24 @@
 package collectionConcept;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class linkedlistPrep {
-	public static void maain(String[] arsg) {
-		System.out.println("Hii");
+	public static void main(String[] arsg) {
+		// mutable list...
+		ArrayList<String> arrList = new ArrayList<String>();
+		arrList.add("Rahul");
+		// I need immutable list..
+		List<String> immuList = List.of("A", "B");// comes in java 9
+		// before java 9 there is arrays but it is Partially mutable
+		String[] arr = { "c", "d" };
+		List<String> listArr = Arrays.asList(arr);
+		listArr.set(0, "e");
+		// you can make list as mutable also
+		ArrayList<String> arrList2 = new ArrayList<>(immuList);
+		arrList2.add("c");
+		System.out.println(arrList2);
 	}
 }
