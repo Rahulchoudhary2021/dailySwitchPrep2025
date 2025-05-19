cfd8f38be1e0d0befb2a60744a4102f8bb840dae
rahul.sureshkumar <rahul.sureshkumar@LPT4711.newgen.co.in>
2025-05-19 10:34:12 +0530

    difference

---
+++ coreJavaConcept/src/collectionConcept/comparatorCompairable.java
@@ -0,0 +1,19 @@
+package collectionConcept;
+
+import java.util.LinkedList;
+
+public class comparatorCompairable {
+
+	public static void main(String args[]) {
+
+		LinkedList<String> list = new LinkedList<>();
+		list.add("apple");
+		list.add("vegetable");
+		list.add("mango");
+		//list.sort(null);
+		list.sort((a,b)->b.length()-a.length());
+		System.out.println(list);
+		
+
+	}
+}
