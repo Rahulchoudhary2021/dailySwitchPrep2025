bd650dcc479e824208a077d1eaebe72aa8040077
rahul.sureshkumar <rahul.sureshkumar@LPT4711.newgen.co.in>
2025-05-25 22:04:54 +0530

    stream

--- coreJavaConcept/src/collectionConcept/streamPrep.java
+++ coreJavaConcept/src/collectionConcept/streamPrep.java
@@ -1,11 +1,24 @@
 package collectionConcept;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class streamPrep{
     public static void main(String args[]){
-        Predicate<Integer>isBoolean=x->x % 2==0;
-        System.out.println(isBoolean.test(10));
+    	
 
+        List<String> names=Arrays.asList("Rahul","Chuoudhary","Angel");
+        
+        //Simple print with stream-----------
+         names.stream().forEach(System.out::println);
+
+        //Doing some operation on input-----------
+        names.stream().filter(values -> values.startsWith("R")).forEach(System.out::println);
+         
+        //Transform input so need to use map--------- 
+         List<String>values=names.stream().map(input ->input.toUpperCase()).collect(Collectors.toList());
+         System.out.println(values);
     }
 }
